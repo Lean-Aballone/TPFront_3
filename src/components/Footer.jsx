9a5fc82ffe5ee774df3f316b94772da71af95ac2
Lean-Aballone <l.aballone@gmail.com>
2025-11-11 17:46:10 -0300

    (footer) Changed: tp2->tp3 + github url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <footer style={{textAlign:"center",color:"var(--text-invert)",background:"var(--footer-gradient)"}}>
-            <small>© 2025 — Grupo 14 — TP02: <a target="_blank" href="https://github.com/Lean-Aballone/TPFront_2"><u>Github</u></a></small>
+            <small>© 2025 — Grupo 14 — TP03: <a target="_blank" href="https://github.com/Lean-Aballone/TPFront_3"><u>Github</u></a></small>
         </footer>
     );
 }
