726870688f008850a65aa94dd69b012f0ca5323c
Lean-Aballone <l.aballone@gmail.com>
2025-11-11 21:59:55 -0300

    Added collapsable articles for bitacora component

--- src/Bitacora/Article.jsx
+++ src/Bitacora/Article.jsx
@@ -1,9 +1,11 @@
-export default function Article({ titulo, fecha, info }){
+export default function Article({ titulo, fecha, info, open = false }){
     return (
         <article>
-            <h3>{titulo}</h3>
-            <h4>{fecha}</h4>
-            <p>{info}</p>            
+            <details open={open}>
+                <summary><h3>{titulo}</h3></summary>
+                <h4>{fecha}</h4>
+                <p>{info}</p>    
+            </details>
         </article>
     );
 }
